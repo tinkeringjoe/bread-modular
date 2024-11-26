89046d866c2eb2dc0d3a38f9a7f931c1051aa415
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2024-11-26 19:10:11 +0530

    Implement the MIDI to Gate setup with ATTiny1616

--- modules/midi/code/esp_main/esp_main.ino
+++ modules/midi/code/esp_main/esp_main.ino
@@ -1,25 +1,41 @@
+/* Arduino ATTiny 1616 Pin Map
+
+  0 - PA4
+  1 - PA5
+  2 - PA6
+  3 - PA7
+  4 - PB5
+  5 - PB4
+  6 - PB3, RX
+  7 - PB2, TX
+  8 - PB1
+  9 - PB0
+  10 - PC0
+  11 - PC1
+  12 - PC2
+  13 - PC3
+  14 - PA1
+  15 - PA2
+  16 - PA3
+*/
+
 #include <MIDI.h>
 
 // Create a MIDI object
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
 
-// Define the LED pin
-const int GATE_PIN_01 = 11;
-const int GATE_PIN_02 = 12;
-const int GATE_PIN_03 = 13;
-const int GATE_PIN_04 = 14;
-const int GATE_PIN_05 = 15;
-const int GATE_PIN_06 = 16;
-const int GATE_PIN_07 = 17;
-const int GATE_PIN_08 = 18;
-const int GATE_PIN_09 = 33;
-const int GATE_PIN_10 = 34;
-const int GATE_PIN_11 = 35;
-const int GATE_PIN_12 = 36;
-const int GATE_PIN_13 = 37;
-const int GATE_PIN_14 = 38;
-const int GATE_PIN_15 = 39;
-const int GATE_PIN_16 = 40;
+#define PIN_RX 6 // PB3
+#define PIN_TX 7 // PB2
+
+// Define the Gate Pins
+#define GATE_PIN_01 10 // PC0
+#define GATE_PIN_02 11 // PC1
+#define GATE_PIN_03 12 // PC2
+#define GATE_PIN_04 13 // PC3
+#define GATE_PIN_05 14 // PA1
+#define GATE_PIN_06 15 // PA2
+#define GATE_PIN_07 16 // PA3
+#define GATE_PIN_08 0 // PA4
 
 int getGatePin(int id) {
   switch (id) {
@@ -39,92 +55,46 @@ int getGatePin(int id) {
       return GATE_PIN_07;
     case 8:
       return GATE_PIN_08;
-    case 9:
-      return GATE_PIN_09;
-    case 10:
-      return GATE_PIN_10;
-    case 11:
-      return GATE_PIN_11;
-    case 12:
-      return GATE_PIN_12;
-    case 13:
-      return GATE_PIN_13;
-    case 14:
-      return GATE_PIN_14;
-    case 15:
-      return GATE_PIN_15;
-    case 16:
-      return GATE_PIN_16;
     default:
       return -1;
   }
 }
 
 void setupGates() {
-  for (int x = 1; x <= 16; x = x + 1) {
+  for (int x = 1; x <= 8; x = x + 1) {
     int gatePin = getGatePin(x);
     pinMode(gatePin, OUTPUT);
     digitalWrite(gatePin, LOW);
   }
 }
 
-void setup() {
-  // Initialize Serial Monitor for debugging
-  Serial.begin(115200);
-  while (!Serial) {
-    delay(10); // Wait for Serial Monitor to open
-  }
-  Serial.println("MIDI Listener Started!");
-  Serial.print("CPU Frequency: ");
-  Serial.print(getCpuFrequencyMhz());
-  Serial.println(" MHz");
-
-  // Set up the LED pin as output
-  setupGates();
-
-  // Initialize UART1 for MIDI communication
-  // Parameters: baud rate, serial config, RX pin, TX pin
-  Serial1.begin(31250, SERIAL_8N1, 42, -1); // RX on GPIO42, no TX
-
-  // Initialize the MIDI library
-  MIDI.begin(MIDI_CHANNEL_OMNI); // Listen to all MIDI channels
-  MIDI.setHandleNoteOn(handleNoteOn);
-  MIDI.setHandleNoteOff(handleNoteOff);
-}
-
-void loop() {
-  // Check for MIDI messages
-  MIDI.read();
-}
-
-// Callback for Note On messages
 void handleNoteOn(byte channel, byte note, byte velocity) {
-  Serial.print("Note On - Channel: ");
-  Serial.print(channel);
-  Serial.print(", Note: ");
-  Serial.print(note);
-  Serial.print(", Velocity: ");
-  Serial.println(velocity);
-
-  // Turn on the LED if Note On is received
-  for (int x = 1; x <= 16; x = x + 1) {
-    int gatePin = getGatePin(x);
+  int gatePin = getGatePin(channel);
+  if (gatePin != -1) {
     digitalWrite(gatePin, HIGH);
   }
 }
 
 // Callback for Note Off messages
 void handleNoteOff(byte channel, byte note, byte velocity) {
-  Serial.print("Note Off - Channel: ");
-  Serial.print(channel);
-  Serial.print(", Note: ");
-  Serial.print(note);
-  Serial.print(", Velocity: ");
-  Serial.println(velocity);
-
-  // Turn off the LED if Note Off is received
-  for (int x = 1; x <= 16; x = x + 1) {
-    int gatePin = getGatePin(x);
+  int gatePin = getGatePin(channel);
+  if (gatePin != -1) {
     digitalWrite(gatePin, LOW);
   }
+}
+
+void setup() {
+  setupGates();
+
+  Serial.begin(31250);
+   // Initialize the MIDI library
+  MIDI.begin(MIDI_CHANNEL_OMNI); // Listen to all MIDI channels
+  MIDI.setHandleNoteOn(handleNoteOn);
+  MIDI.setHandleNoteOff(handleNoteOff);
+
+  Serial.println("MIDI Listening Started!");
+}
+
+void loop() {
+  MIDI.read();
 }
