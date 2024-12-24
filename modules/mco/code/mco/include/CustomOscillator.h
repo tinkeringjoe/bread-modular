47c25560a40238099ea9845f87ebc791e7d178e8
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2024-12-25 00:26:48 +0530

    Introduce wavetable generator

---
+++ modules/mco/code/mco/include/CustomOscillator.h
@@ -0,0 +1,36 @@
+#ifndef CUSTOM_OSCILLATOR_H
+#define CUSTOM_OSCILLATOR_H
+
+#include <stdint.h>
+
+class CustomOscillator {
+public:
+    CustomOscillator(const int8_t* wavetable, uint16_t tableSize, uint32_t sampleRate)
+        : wavetable(wavetable), tableSize(tableSize), sampleRate(sampleRate), phase(0), phaseIncrement(0) {}
+
+    // Set the desired frequency
+    void setFrequency(float frequency) {
+        // Calculate the phase increment based on frequency, table size, and sample rate
+        phaseIncrement = (frequency * tableSize) / sampleRate;
+    }
+
+    // Generate the next sample
+    int8_t next() {
+        // Calculate the current index in the wavetable
+        uint16_t index = (uint16_t)phase & (tableSize - 1); // Use upper 8 bits of 32-bit phase accumulator
+        // Increment the phase accumulator
+        phase += phaseIncrement;
+
+        // Return the wavetable value at the current index
+        return wavetable[index];
+    }
+
+private:
+    const int8_t* wavetable;  // Pointer to the wavetable
+    uint16_t tableSize;        // Size of the wavetable
+    uint32_t sampleRate;       // Sample rate in Hz
+    uint32_t phase;            // Phase accumulator
+    uint32_t phaseIncrement;   // Phase increment based on frequency
+};
+
+#endif
--- modules/mco/code/mco/src/main.cpp
+++ modules/mco/code/mco/src/main.cpp
@@ -2,37 +2,38 @@
 #include <SoftwareSerial.h>
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <MIDI.h>
 #include "utils.h"
+#include "SimpleMIDI.h"
+#include "CustomOscillator.h"
 
 #define GATE_PIN PIN_PA7
 #define LOGGER_PIN_TX PIN_PB4
 
+SimpleMIDI MIDI;
 SoftwareSerial logger = SoftwareSerial(-1, LOGGER_PIN_TX);
 
-// Create a MIDI object
-MIDI_CREATE_INSTANCE(HardwareSerial, Serial, MIDI);
+const int8_t sineTable[256] = {
+0, 3, 6, 9, 12, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 51, 54, 57, 60,
+63, 65, 68, 71, 73, 76, 78, 81, 83, 85, 88, 90, 92, 94, 96, 98, 100, 102, 104,
+106, 107, 109, 111, 112, 113, 115, 116, 117, 118, 120, 121, 122, 122, 123, 124,
+125, 125, 126, 126, 126, 127, 127, 127, 127, 127, 127, 127, 126, 126, 126, 125,
+125, 124, 123, 122, 122, 121, 120, 118, 117, 116, 115, 113, 112, 111, 109, 107,
+106, 104, 102, 100, 98, 96, 94, 92, 90, 88, 85, 83, 81, 78, 76, 73, 71, 68, 65,
+63, 60, 57, 54, 51, 49, 46, 43, 40, 37, 34, 31, 28, 25, 22, 19, 16, 12, 9, 6, 3,
+0, -3, -6, -9, -12, -16, -19, -22, -25, -28, -31, -34, -37, -40, -43, -46, -49,
+-51, -54, -57, -60, -63, -65, -68, -71, -73, -76, -78, -81, -83, -85, -88, -90,
+-92, -94, -96, -98, -100, -102, -104, -106, -107, -109, -111, -112, -113, -115,
+-116, -117, -118, -120, -121, -122, -122, -123, -124, -125, -125, -126, -126,
+-126, -127, -127, -127, -127, -127, -127, -127, -126, -126, -126, -125, -125,
+-124, -123, -122, -122, -121, -120, -118, -117, -116, -115, -113, -112, -111,
+-109, -107, -106, -104, -102, -100, -98, -96, -94, -92, -90, -88, -85, -83, -81,
+-78, -76, -73, -71, -68, -65, -63, -60, -57, -54, -51, -49, -46, -43, -40, -37,
+-34, -31, -28, -25, -22, -19, -16, -12, -9, -6, -3,
+ };
 
-void handleNoteOn(byte channel, byte note, byte velocity) {
-  const int freq = midiToFrequency(note);
-  digitalWrite(GATE_PIN, HIGH);
+CustomOscillator oscillator(sineTable, 256, 10000);
 
-  logger.printf("NOTE ON: %d\n", note);
-
-  // Send the velocity out via PA6
-  DAC0.DATA = (velocity / 127.0) * 255;
-}
-
-void handleNoteOff(byte channel, byte note, byte velocity) {
-  digitalWrite(GATE_PIN, LOW);
-
-  logger.printf("NOTE OFF: %d\n", note);
-
-  // Reset the velocity to 0.
-  DAC0.DATA = 0;
-}
-
-void setupTone1() {
+void setupTimer() {
     TCB0.CTRLA |= TCB_ENABLE_bm; // counting value
 
     // Uses 20Mhz clock & divide it by for here
@@ -49,17 +50,11 @@ void setupTone1() {
 }
 
 // TCB0 Interrupt Service Routine
-int val = 0;
 ISR(TCB0_INT_vect) {
-  if (val == 0) {
-    val = 1;
-    DAC0.DATA = 0;
-  } else {
-    val = 0;
-    DAC0.DATA = 255;
-  }
+  // Output sine wave to DAC
+  DAC0.DATA = oscillator.next() + 127;
 
-  // Clear the interrupt flag
+  // Clear the interrupt flag∫
   TCB0.INTFLAGS = TCB_CAPT_bm;
 }
 
@@ -68,21 +63,10 @@ void setup() {
   pinMode(GATE_PIN, OUTPUT);
   digitalWrite(GATE_PIN, LOW);
 
-  // Setup Serial Comm
-  Serial.begin(31250);
-
   // Setup Logger
   pinMode(LOGGER_PIN_TX, OUTPUT);
   logger.begin(9600);
   
-  // Initialize the MIDI library
-  // Listen to only the channel 1
-  MIDI.begin(1);
-  MIDI.setHandleNoteOn(handleNoteOn);
-  MIDI.setHandleNoteOff(handleNoteOff);
-  logger.println("MCO 0.2.0 Started!");
-
-
   // Set the analog reference for ADC with supply voltage.
   analogReference(VDD);
  
@@ -92,10 +76,33 @@ void setup() {
   DAC0.CTRLA = DAC_ENABLE_bm | DAC_OUTEN_bm;
   DAC0.DATA = 0;
 
-  setupTone1();
+  // Setup MIDI
+  MIDI.begin(31250);
+
+  setupTimer();
+  oscillator.setFrequency(1000);
+
+  logger.println("MCO 0.2.0 Started!");
 }
 
 void loop() {
   // parse incoming MIDI messages
-  MIDI.read();
+   if (MIDI.read()) {
+      uint8_t type = MIDI.getType();
+      uint8_t channel = MIDI.getChannel();
+      uint8_t data1 = MIDI.getData1();
+      uint8_t data2 = MIDI.getData2();
+
+      // Debug output
+      if (type == MIDI_NOTE_ON) {
+        float freq = midiToFrequency(data1);
+        oscillator.setFrequency(freq);   
+      } else if (type == MIDI_NOTE_OFF) {
+          
+      } else if (type == MIDI_CONTROL_CHANGE) {
+          
+      } else {
+          logger.print("Unknown | ");
+      }
+  }
 }
