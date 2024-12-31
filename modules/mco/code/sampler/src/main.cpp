bee7370aec3ddfb613a52d7cce272c44e915b0cc
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2025-01-01 00:19:56 +0530

    Add the basic kick sampler

---
+++ modules/mco/code/sampler/src/main.cpp
@@ -0,0 +1,125 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "utils.h"
+#include "SimpleMIDI.h"
+#include "CustomOscillator.h"
+#include "ModeHandler.h"
+#include "samples/kick.h"
+
+#define GATE_PIN PIN_PA7
+#define LOGGER_PIN_TX PIN_PB4
+#define PIN_CV1 PIN_PA1
+#define PIN_CV2 PIN_PA2
+#define TOGGLE_PIN PIN_PC2
+#define TOGGLE_LED PIN_PC3
+
+SimpleMIDI MIDI;
+SoftwareSerial logger = SoftwareSerial(-1, LOGGER_PIN_TX);
+ModeHandler modes = ModeHandler(TOGGLE_PIN, 3, 300);
+
+void toggleLED() {
+  delay(500);
+  digitalWrite(TOGGLE_LED, HIGH);
+  delay(500);
+  digitalWrite(TOGGLE_LED, LOW);
+}
+
+void setupTimer() {
+    TCB0.CTRLA |= TCB_ENABLE_bm; // counting value
+
+    // Uses 20Mhz clock & divide it by for here
+    // So, 1 tick is 0.1us
+    TCB0.CTRLA |= TCB_CLKSEL_CLKDIV2_gc; // clock div by 2
+
+    TCB0.CCMP = 10000000 / KICK_SAMPLE_LENGTH; // Set the compare value
+  
+    // Enabling inturrupts
+    TCB0.CTRLB |= TCB_CNTMODE_INT_gc; // Timer interrupt mode (periodic interrupts)
+    TCB0.INTCTRL = TCB_CAPT_bm; // Enable Capture interrupt
+    sei();
+}
+
+int index = 0;
+// TCB0 Interrupt Service Routine
+ISR(TCB0_INT_vect) {
+  if (index < KICK_SAMPLE_LENGTH) {
+    DAC0.DATA = KICK_SAMPLE[index];
+    index += 1;
+  }
+
+
+  // Clear the interrupt flag∫
+  TCB0.INTFLAGS = TCB_CAPT_bm;
+}
+
+void setup() {
+  // define the gate pin
+  pinMode(GATE_PIN, OUTPUT);
+  digitalWrite(GATE_PIN, LOW);
+
+  // Setup Logger
+  pinMode(LOGGER_PIN_TX, OUTPUT);
+  logger.begin(9600);
+  
+  // Set the analog reference for ADC with supply voltage.
+  analogReference(VDD);
+  pinMode(PIN_CV1, INPUT);
+  pinMode(PIN_CV2, INPUT);
+ 
+  // DAC0 setup for sending velocity via the PA6 pin
+  VREF.CTRLA |= VREF_DAC0REFSEL_4V34_gc; //this will force it to use VDD as the VREF
+  VREF.CTRLB |= VREF_DAC0REFEN_bm;
+  DAC0.CTRLA = DAC_ENABLE_bm | DAC_OUTEN_bm;
+  DAC0.DATA = 0;
+
+  // Setup MIDI
+  MIDI.begin(31250);
+
+  // Timer related
+  setupTimer();
+  
+  // Modes
+  modes.begin();
+  pinMode(TOGGLE_LED, OUTPUT);
+
+  logger.println("MCO 0.2.0 Started!");
+}
+
+void loop() {
+  // parse incoming MIDI messages
+   if (MIDI.read()) {
+      uint8_t type = MIDI.getType();
+      uint8_t channel = MIDI.getChannel();
+      uint8_t data1 = MIDI.getData1();
+      uint8_t data2 = MIDI.getData2();
+
+      // Debug output
+      if (type == MIDI_NOTE_ON) {
+        index = 0;
+        digitalWrite(GATE_PIN, HIGH);
+      } else if (type == MIDI_NOTE_OFF) {
+        digitalWrite(GATE_PIN, LOW);
+      } else if (type == MIDI_CONTROL_CHANGE) {
+          
+      } else {
+          // logger.print("Unknown MIDI Data| ");
+      }
+  }
+
+  // mode related code
+  if (modes.update()) {
+    byte currentMode = modes.getMode();
+    if (currentMode == 0) {
+      toggleLED();
+    } else if (currentMode == 1) {
+      toggleLED();
+      toggleLED();
+    } else if (currentMode == 2) {
+      toggleLED();
+      toggleLED();
+      toggleLED();
+    }
+  }
+}
